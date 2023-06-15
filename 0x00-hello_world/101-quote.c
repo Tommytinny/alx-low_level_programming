da34d399ddad70f0d585cf19f848ac9af55c4b17
Akintomide Akinola <tommyteenboi@gmail.com>
2023-06-15 15:09:07 -0700

    added 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 
 /**
  * main - program that prints a line into standard error
@@ -7,6 +6,6 @@
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", stdout);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", stderr);
 return (1);
 }
