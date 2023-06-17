3f08080e45828b8764b0004b721fa69de335fa81
Akintomide Akinola <tommyteenboi@gmail.com>
2023-06-16 17:48:11 -0700

    added 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints all possible combination of single-digit numbers
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+		putchar(number + '0');
+		
+		if (number == 9)
+		{
+			break;
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -16,3 +16,5 @@ This C program prints all single digit numbers of base 10 starting from 0
 This C program prints the lowercase alphabet in reverse
 # File: 8-print_base16.c
 This C program prints all the numbers of base 16 in lowercase
+# File: 9-print_comb.c
+This C program prints all possible combinations of single-digit numbers.
