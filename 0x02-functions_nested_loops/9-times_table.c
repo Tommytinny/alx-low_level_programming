c09ae7fa83876135221b7f782d3373ce344932e3
Akintomide Akinola <tommyteenboi@gmail.com>
2023-06-21 08:43:16 -0700

    fix error

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,9 +23,13 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 			}
-			if (product != 0 && product < 10)
+			if (product < 10)
 			{
 				_putchar(' ');
+			}
+			else if (product == 0)
+			{
+				_putchar('');
 			}
 			else
 			{
