b0ef33bf47aa2377d8104e9db482e5fc0b7ee39d
Akintomide Akinola <tommyteenboi@gmail.com>
2023-06-20 17:57:22 -0700

    added file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if character is uppercase or lowercase
+ * @c: character to check
+ * Return: 1 if meet requirement otherwise 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
