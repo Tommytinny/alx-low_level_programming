f4827c51821cc6708c8bfd5cd09f0787ff7330f6
Akintomide Akinola <tommyteenboi@gmail.com>
2023-06-20 17:15:37 -0700

    fixing error

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * _islower - check if lowercase
- * Return: 1 if c is lowercase otherwise 0
+ * _islower - check if c is lowercase
+ * Description: c represent the alphabet
+ * Return: 1 if c is lowercase
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
 }
