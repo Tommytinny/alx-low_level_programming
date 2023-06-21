bb0742882de131d0e69825865cdd4f73e1f200bf
Akintomide Akinola <tommyteenboi@gmail.com>
2023-06-21 02:31:21 -0700

    fixing error

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,9 +8,9 @@ void jack_bauer(void)
 {
 	int hrs, mins;
 
-	for (hrs = 0; hrs < 24, hrs++)
+	for (hrs = 0; hrs < 24; hrs++)
 	{
-		for (mins = 0; mins < 60, mins++)
+		for (mins = 0; mins < 60; mins++)
 		{
 			_putchar((hrs / 10) + '0');
 			_putchar((hrs % 10) + '0');
