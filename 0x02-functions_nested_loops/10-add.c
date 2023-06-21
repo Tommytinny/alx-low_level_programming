7f3ff1dba9e2a193ac5d2e7c4a886d2078e84f4c
Akintomide Akinola <tommyteenboi@gmail.com>
2023-06-21 16:48:39 -0700

    added file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add -  add two numbers together
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
