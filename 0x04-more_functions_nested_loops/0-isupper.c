3d65a9329dc60c9a186a59e0272762c45cb4c404
Akintomide Akinola <tommyteenboi@gmail.com>
2023-06-22 14:52:47 -0700

    added file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - Functions that checks uppercase character
+ * @c: character to check
+ * Return: 1 if uppercase 0 if not
+ */
+int _isupper(int c)
+{
+	if ((c >= 65) && (c <= 90))
+	{
+		return (1);
+	}
+	return (0);
+}
