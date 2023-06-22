bc63b68e397584e31a0b7c9846f3a59e4024d262
Akintomide Akinola <tommyteenboi@gmail.com>
2023-06-22 15:32:30 -0700

    added file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integer
+ * @a: first character
+ * @b: second character
+ * Return: the product
+ */
+int mul(int a, int b)
+{
+	int product = a * b;
+
+	return (product);
+}
