7a655d2a2f6e4bcc92754e260afb9c6d3c6b3f8d
Akintomide Akinola <tommyteenboi@gmail.com>
2023-06-23 01:55:29 -0700

    added file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - print number of lines inputed
+ * @n: Numbers of line to be print
+ */
+void print_line(int n)
+{
+	int line;
+
+	for (start = 0; start < n; start++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
