8bd76a8adaeb274acf7975d05815ee5056d15129
Akintomide Akinola <tommyteenboi@gmail.com>
2023-06-23 17:28:53 -0700

    fixing

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,12 +1,11 @@
-#include "main.h"
-
+#include <stdio.h>
 /**
- * printFizzBuzz - function
+ * main - entry point
  *
  * Return: void
  */
 
-int printFizzBuzz(void)
+int main(void)
 {
 	int i;
 
@@ -14,21 +13,21 @@ int printFizzBuzz(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			_putchar("FizzBuzz ");
+			printf("FizzBuzz ");
 		}
 		else if (i % 3 == 0)
 		{
-			_putchar("Buzz ");
+			printf("Buzz ");
 		}
 		else if (i % 5 == 0)
 		{
-			_putchar("Buzz ");
+			printf("Buzz ");
 		}
 		else
 		{
-			_putchar("%d ", i);
+			printf("%d ", i);
 		}
 	}
-	_putchar("\n");
+	printf("\n");
 	return (0);
 }
