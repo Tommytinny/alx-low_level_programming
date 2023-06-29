6a8ddf684ad26961ea3ec9984c143c26b3cfe88d
Akintomide Akinola <tommyteenboi@gmail.com>
2023-06-29 12:26:47 -0700

    added new file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * Return: string
+ */
+char *_strcat(char *dest, char *src)
+{
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		*dest++;
+		*src++;
+	}
+
+	*dest = '\0';
+
+	return (dest);
+}
