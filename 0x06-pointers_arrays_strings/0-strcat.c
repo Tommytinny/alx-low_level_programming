6aa68fd20415fd729ae338aa22a29de024752c94
Akintomide Akinola <tommyteenboi@gmail.com>
2023-06-29 13:05:08 -0700

    finally fixed the error

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,19 +9,21 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	while (*dest != '\0')
+	char *ptr = dest;
+	/* move the pointer to the end of the destination string*/
+	while (*ptr != '\0')
 	{
-		dest++;
+		ptr++;
 	}
 
 	while (*src != '\0')
 	{
-		*dest = *src;
-		dest++;
+		*ptr = *src;
+		ptr++;
 		src++;
 	}
 
-	*dest = '\0';
+	*ptr = '\0';
 
 	return (dest);
 }
