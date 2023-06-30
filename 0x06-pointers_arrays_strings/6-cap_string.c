4fce9f776f51e2bed598b24ff93081abc4674db0
Akintomide Akinola <tommyteenboi@gmail.com>
2023-06-29 18:17:58 -0700

    added new file

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *cap_string - capitalizes all words of a string
+ * @str: string
+ * Return: 0
+ */
+char *cap_string(char *str)
+{
+	int capitalize_next = 1;
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if (*ptr == ' ' || *ptr == '\t' || *ptr == '\n' || *ptr == ',' ||
+            *ptr == ';' || *ptr == '.' || *ptr == '!' || *ptr == '?' ||
+            *ptr == '"' || *ptr == '(' || *ptr == ')' || *ptr == '{' ||
+            *ptr == '}')
+		{
+			capitalize_next = 1;
+		}
+		else if (capitalize_next && islower(*ptr))
+		{
+			*ptr = toupper(*ptr);
+			capitalize_next = 0;
+		}
+
+		ptr++;
+	}
+
+	return (str);
+}
+				
