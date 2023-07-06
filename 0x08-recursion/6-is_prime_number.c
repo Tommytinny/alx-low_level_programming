47fd16034ec8946643eb21812ea1baa5477433fe
Akintomide Akinola <tommyteenboi@gmail.com>
2023-07-06 16:21:28 -0700

    add file

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -21,20 +21,17 @@ int primenum(int a, int b)
 }
 
 /**
- * primenum - Checks from 1  to n
- * @a: place holder for n
- * @b: number that iterates from 1 to n
- * Return: On success 1
+ * is_prime_number - prime number
+ * @n: input value
+ *
+ * Return: 1 if n is prime number else 0
  */
-int primenum(int a, int b)
+int is_prime_number(int n)
 {
-	if (a == b)
-	{
-		return (1);
-	}
-	else if (a % b == 0)
+	if (n <= 1)
 	{
 		return (0);
 	}
-	return (primenum(a, b + 1));
+	return (primenum(n, 2));
 }
+
