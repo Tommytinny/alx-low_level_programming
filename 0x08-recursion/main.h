92006f16dd5869ff754e0e0e1d94ebaa6c9ac691
Akintomide Akinola <tommyteenboi@gmail.com>
2023-07-06 15:18:02 -0700

    fixed

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,5 +8,5 @@ int _strlen_recursion(char *s);
 int _sqrt_recursion(int n);
 int factorial(int n);
 int _pow_recursion(int x, int y);
-int cal_sqrt(int n, s, e);
+int cal_sqrt(int n, int s, int e);
 #endif
