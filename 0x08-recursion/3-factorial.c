76517ea16f7affc2693c3f57dfa8d2b213aba4f6
Akintomide Akinola <tommyteenboi@gmail.com>
2023-07-07 00:34:03 -0700

    fixed error

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,17 +1,15 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-<<<<<<< HEAD
  * factorial -  returns the factorial of a given number.
  * @n: given number
  * Return: the factorial of the given number
-=======
  * factorial - returns the factorial of a given number
  * @n: Given number
  *
  * Return: if n is less than 0, return -1.
  * if n is equal to 0, return 1.
->>>>>>> 35aabcae79cef8f2f4a62ea0d599a91b1a972117
  */
 int factorial(int n)
 {
@@ -19,9 +17,7 @@ int factorial(int n)
 	{
 		return (-1);
 	}
-<<<<<<< HEAD
 	return (n * factorial(n - 1));
-=======
 	else if (n == 0)
 	{
 		return (1);
@@ -30,5 +26,4 @@ int factorial(int n)
 	{
 		return (n * factorial(n - 1));
 	}
->>>>>>> 35aabcae79cef8f2f4a62ea0d599a91b1a972117
 }
