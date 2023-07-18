83501d7d153771a3095b023c860cd9fa81f47c25
Akintomide Akinola <tommyteenboi@gmail.com>
2023-07-18 13:43:40 -0700

    added new file

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - free the dogs
+ * @d: pointer to dog_t struct
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
