6153b15d3dbb9ebf9e7ac8a450a436219725df97
Akintomide Akinola <tommyteenboi@gmail.com>
2023-07-19 16:47:25 -0700

    fixed error

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,11 +13,13 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	if (array == NULL || size == NULL || action == NULL)
+	if (array == NULL || action == NULL)
 		return;
 
-	while (array[i] < size)
-		action(array[i++]);
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
