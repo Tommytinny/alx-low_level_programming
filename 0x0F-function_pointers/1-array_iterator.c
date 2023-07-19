1e86d52c494a2b3890c87392ce0edb085139a213
Akintomide Akinola <tommyteenboi@gmail.com>
2023-07-19 16:21:49 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,6 +8,7 @@
  * @array: pointer to the array
  * @size: number of elements in the array
  * @action: pointer to the funtion
+ * Return: nothing
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
