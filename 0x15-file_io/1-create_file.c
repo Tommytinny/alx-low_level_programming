faeceaa9c2fa37a81d4e207418f143ea40d170bb
Tommytinny <tommyteenboi@gmail.com>
2023-08-07 22:13:24 +0100

    added new

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,7 +33,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	
+
 	fileDes = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (fileDes == -1)
 	{
@@ -46,7 +46,7 @@ int create_file(const char *filename, char *text_content)
 		if (byteW == -1)
 			return (-1);
 	}
-	
+
 	close(fileDes);
 
 	return (1);
