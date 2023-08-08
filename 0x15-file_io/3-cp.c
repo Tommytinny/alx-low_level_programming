5b2160bf093c1cf0ef9f421df026faa49d749ec2
Tommytinny <tommyteenboi@gmail.com>
2023-08-08 22:23:46 +0100

    out

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -44,7 +44,7 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
-	fd_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0666);
+	fd_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT | O_APPEND, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
