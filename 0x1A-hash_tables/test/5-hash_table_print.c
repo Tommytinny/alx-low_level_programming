bf1042c9b5ecbe5ac06f41fc1bde6eed8be85624
Tommytinny <tommyteenboi@gmail.com>
2023-11-03 22:04:06 +0100

    added files

---
+++ 0x1A-hash_tables/test/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that prints a hash table
+ * @ht: is the hash table
+ *
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	long unsigned int i;
+	hash_node_t *items;
+	int printed;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		items = ht->array[i];
+		while (items != NULL)
+		{
+			if (printed)
+			{
+				printf(", ");
+			}
+			printf("'%s': '%s'", items->key, items->value);
+			printed = 1;
+			items = items->next;
+		}
+	}
+	printf("}\n");
+}
