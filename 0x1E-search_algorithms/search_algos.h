6e9be5ea742a744a2480c93e880b337806eda483
Tommytinny <tommyteenboi@gmail.com>
2024-02-18 07:31:30 -0500

    betty description

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -25,7 +25,7 @@ typedef struct skiplist_s
  * struct listint_s - singly linked list
  * @n: Integer
  * @index: Index of the node in the list
- *
+ * @next: Pointer to the next node
  * Description: singly linked list node structure
  * for Holberton project
  */
